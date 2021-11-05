46214219cb70cd7e91526313f424fab6d13a2399
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-11-05 14:22:34 +0300

    feat:add file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
-<template>Hi
+<template>
+<div class="app">
+  <about/>
+  <tea/>
+  <selection/>
+  <feedback/>
+  <contacts/>
+  <account/>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import About from './components/About.vue'
+import Tea from './components/Tea.vue'
+import Selection from './components/Selection.vue'
+import Feedback from './components/Feedback.vue'
+import Contacts from './components/Contacts.vue'
+import Account from './components/Account.vue'
+
+
+
+
 
 export default defineComponent({
   name: 'App',
   components: {
+    About, Tea, Selection, Feedback, Contacts, Account
   },
 })
 </script>
