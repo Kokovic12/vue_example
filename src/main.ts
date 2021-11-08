afd5df572bb42ca04b33971ff4180616384c70e6
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-11-08 08:27:38 +0300

    add vue-material

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App).use(VueMaterial).mount('#app')
