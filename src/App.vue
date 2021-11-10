cc01b2f960229838ba5d2b85e94d7b6f0d7337d0
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-11-10 16:26:31 +0300

    featЖ training

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,8 @@
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
+        <!-- <button class="btn btn-outline-success" @click="$router.push('/chat')" >Search</button>  обвес событием через роутинг, альтернатива тегу  router-link-->
+
       </form>
     </div>
   </div>
@@ -50,7 +52,6 @@
 
   <router-view/>
 </template>
-
 <style lang="scss">
 
 </style>
