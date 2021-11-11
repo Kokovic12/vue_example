c483dc5ccfd1395f45d71a15f450a3cb2bf3f7ba
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-11-11 12:32:58 +0300

    feat:add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
+import store from '@/store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).use(BootstrapVue3).mount('#app')
+createApp(App).use(router).use(BootstrapVue3).use(store).mount('#app')
