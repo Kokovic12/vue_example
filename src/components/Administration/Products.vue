d21f0ace7a1a595bccb20d2c41491cca8bd92d7b
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-12-20 17:59:04 +0300

    feat:table

--- src/components/Administration/Products.vue
+++ src/components/Administration/Products.vue
@@ -19,17 +19,17 @@
     <table class="table table-striped">
       <thead>
         <tr>
-          <th>
+          <th >
             <div class="d-flex flex-row">
               <input v-model="idTeaFilter" class="form-control m-2" placeholder="Filter number" @keyup="FilterFn()">
 
-              <button type="button" class="btn btn-light" @click="sortResult('numberTea',true)">
+              <button type="button" class="btn btn-light" @click="sort('numberTea',true)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
                 </svg>
               </button>
 
-              <button type="button" class="btn btn-light" @click="sortResult('numberTea',false)">
+              <button type="button" class="btn btn-light" @click="sort('numberTea',false)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                 </svg>
@@ -38,7 +38,7 @@
             </div>
             Number
           </th>
-          <th>
+          <th @click="sort('nameTea')">
             <div class="d-flex flex-row">
               <input v-model="nameTeaFilter" icon='search' class="form-control m-2" placeholder="Filter name" @keyup="FilterFn()">
 
@@ -57,10 +57,9 @@
             </div>
             Name of tea
           </th>
-          <th>
+          <th @click="sort('sortTea')">
             <div class="d-flex flex-row">
               <input v-model="sortTeaFilter" class="form-control m-2" placeholder="Filter sort" @keyup="FilterFn()">
-
 
               <button type="button" class="btn btn-light" @click="sortResult('sortTea',true)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
@@ -77,7 +76,7 @@
             </div>            
             Sort
           </th>
-          <th>
+          <th @click="sort('priceTea')">
             <div class="d-flex flex-row">
               <input v-model="priceTeaFilter" icon='search' class="form-control m-2" placeholder="Filter price" @keyup="Filter()">
 
@@ -103,7 +102,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in products" :key="product.id">
+        <tr v-for="(product, index) in sortedTeas" :key="product.id">
           <td>{{product.idTea}}</td>
           <td >{{product.nameTea}}</td> 
           <td >{{product.sortTea}}</td>      
@@ -116,7 +115,7 @@
               <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
             </button>
-            <button type="button" class="btn btn-light mr-1" @click="deleteClick(product.idTea)">
+            <button type="button" class="btn btn-light mr-1" @click="deleteClick(index)">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
               <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
               </svg>
@@ -158,9 +157,6 @@
 
 
 <script>
-// import { computed } from 'vue'
-// import { axios } from 'axios'
-
 // const performSearch = (rows, term) => {
 //   const results = rows.filter(
 //     row => row.join(' ').toLowerCase().includes(term.toLowerCase())
@@ -168,9 +164,13 @@
 //   return results
 // }
 
+// const sortBynameTea = (d1,d2)=>(d1.nameTea.toLowerCase()>d2.nameTea.toLowerCase())?1:-1,
 export default {
   data() {
     return {
+      teas:[],
+      currentSort:'name',
+      currentSortDir:'asc',
       modalTitle:'',
       idTea: 0,
       nameTea:'',
@@ -182,7 +182,6 @@ export default {
       sortTeaFilter:'',
       priceTeaFilter:'',
       withoutFilter:[],
-      // API_URL:'http://localhost:49146/api/',
       products : [
         { idTea: 1, nameTea: 'Alishan', sortTea: 'oolong', priceTea: 5, descriptionTea:'some description' },
         { idTea: 2, nameTea: 'Da Hong pao', sortTea: 'oolong', priceTea: 10, descriptionTea:'some description' },
@@ -192,10 +191,27 @@ export default {
       ],
     }
   },
-  mounted() {
+  computed:{
+    sortedTeas() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.products.sort((a,b)=>{
+        let modifier = 1
+        if(this.currentSortDir==='desc')modifier = -1
+        if(a[this.currentSort] < b[this.currentSort]) return -1*modifier
+        if(a[this.currentSort] > b[this.currentSort]) return 1*modifier
+        return 0
+      })
+    }
   },
 
   methods:{
+
+    sort(s) {
+      if(s=== this.currentSort) {
+        this.currentSortDir=this.currentSortDir==='asc'?'desc':'asc'
+      }
+      this.currentSort=s
+    },
 
     addClick() {
       this.modalTitle='Add Tea'
@@ -218,6 +234,7 @@ export default {
     createClick() {
       const { products, nameTea, sortTea, priceTea, descriptionTea } = this
       this.products = [...products, {
+        idTea: this.products.length +1,
         nameTea,
         sortTea,
         priceTea,
@@ -227,33 +244,33 @@ export default {
     updateClick(id) {
       //  code
     },
-    deleteClick() {
-      // code
+    deleteClick(index) {
+      this.products.pop(index, 1)
     },
 
     // computed:{
-    FilterFn() {
-      const { idTeaFilter, nameTeaFilter, sortTeaFilter, priceTeaFilter } = this
+    //   FilterFn() {
+    //     const { idTeaFilter, nameTeaFilter, sortTeaFilter, priceTeaFilter } = this
 
-      this.products=this.withoutFilter.filter(
-        (el) =>(el.idTea.toString().toLowerCase().includes(idTeaFilter.toString().trim().toLowerCase())
-          &&
-          el.nameTea.toString().toLowerCase().includes(nameTeaFilter.toString().trim().toLowerCase())
-          &&
-          el.sortTea.toString().toLowerCase().includes(sortTeaFilter.toString().trim().toLowerCase())
-          &&
-          el.priceTea.toString().toLowerCase().includes(priceTeaFilter.toString().trim().toLowerCase()))
-      )
-      // },
-    },
+    //     this.products=this.withoutFilter.filter(
+    //       (el) =>(el.idTea.toString().toLowerCase().includes(idTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.nameTea.toString().toLowerCase().includes(nameTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.sortTea.toString().toLowerCase().includes(sortTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.priceTea.toString().toLowerCase().includes(priceTeaFilter.toString().trim().toLowerCase()))
+    //     )
+    //   },
+    // },
 
-    Filter() {
-      const pr=this.priceTea
-      return this.products.filter((elem) => {
-        if(pr==='') return true
-        return elem.priceTea.indexOf(pr) > -1
-      })
-    }
+    // Filter() {
+    //   const pr=this.priceTea
+    //   return this.products.filter((elem) => {
+    //     if(pr==='') return true
+    //     return elem.priceTea.indexOf(pr) > -1
+    //   })
+    // }
 
 
     // sortResult(prop,asc) {
