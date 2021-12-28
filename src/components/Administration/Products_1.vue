bb0f01d861f77d95f69b0f21c9f21e10709fcbeb
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-12-28 19:58:34 +0300

    feat:change table

--- src/components/Administration/Products_1.vue
+++ src/components/Administration/Products_1.vue
@@ -1,3 +1,349 @@
+
+<template>
+  <div>
+
+    <button type="button" class="btn btn-primary m-2 fload-end" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="addClick()"> Add Tea</button>
+
+      <!-- <div id="demo">
+      <form id="search">
+        Search <input v-model="searchQuery" name="query" />
+      </form>
+      <demo-grid
+        :teas="products"
+        :columns="gridColumns"
+        :filter-key="searchQuery"
+      >
+      </demo-grid>
+    </div> -->
+      
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th @click="sort('idTea')">
+            <div class="d-flex flex-row">
+              <input v-model="idTeaFilter" class="form-control m-2" placeholder="Filter number" @keyup="FilterFn()">
+
+              <button type="button" class="btn btn-light" @click="sortResult('idTea',true)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </button>
+
+              <button type="button" class="btn btn-light" @click="sortResult('idTea',false)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                </svg>
+              </button>
+
+            </div>
+            Number
+          </th>
+          <th @click="sort('nameTea')">
+            <div class="d-flex flex-row">
+              <input v-model="nameTeaFilter" icon='search' class="form-control m-2" placeholder="Filter name" @keyup="FilterFn()">
+
+              <button type="button" class="btn btn-light" @click="sortResult('nameTea',true)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </button>
+
+              <button type="button" class="btn btn-light" @click="sortResult('nameTea',false)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                </svg>
+              </button>
+              
+            </div>
+            Name of tea
+          </th>
+          <th @click="sort('sortTea')">
+            <div class="d-flex flex-row">
+              <input v-model="sortTeaFilter" class="form-control m-2" placeholder="Filter sort" @keyup="FilterFn()">
+
+              <button type="button" class="btn btn-light" @click="sortResult('sortTea',true)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </button>
+
+              <button type="button" class="btn btn-light" @click="sortResult('sortTea',false)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                </svg>
+              </button>
+              
+            </div>            
+            Sort
+          </th>
+          <th @click="sort('priceTea')">
+            <div class="d-flex flex-row">
+              <input v-model="priceTeaFilter" icon='search' class="form-control m-2" placeholder="Filter price" @keyup="Filter()">
+
+              <button type="button" class="btn btn-light" @click="sortResult('priceTea',true)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </button>
+
+              <button type="button" class="btn btn-light" @click="sortResult('priceTea',false)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                </svg>
+              </button>
+              
+            </div>
+            Price
+          </th>
+          <th>
+            Discription
+          </th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in sortedTeas" :key="product.id">
+          <td>{{product.idTea}}</td>
+          <td >{{product.nameTea}}</td> 
+          <td >{{product.sortTea}}</td>      
+          <td >{{product.priceTea}}</td>      
+          <td >{{product.descriptionTea}}</td>  
+          <td>
+            <button type="button" class="btn btn-light mr-1" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="editClick(product)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>
+            </button>
+            <button type="button" class="btn btn-light mr-1" @click="deleteClick(index)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+              </svg>
+            </button>
+            </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div id="exampleModal" class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 id="exampleModalLabel" class="modal-title">{{modalTitel}}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+        <div class="modal-body">
+          <div class="input-group mb-3"></div>
+            <span class="input-group-text">name of Tea</span>
+            <input v-model="nameTea" type="text" class="form-control">
+            <span class="input-group-text">sort of Tea</span>
+            <input v-model="sortTea" type="text" class="form-control">
+            <span class="input-group-text">price of Tea</span>
+            <input v-model="priceTea" type="text" class="form-control">
+            <span class="input-group-text">description of Tea</span>
+            <input v-model="descriptionTea" type="text" class="form-control">
+        </div>
+
+        <button v-if="idTea==0" type="button" class="btn btn-primary m-2 fload-end"  @click="createClick()">Create</button>
+        <button v-if="idTea!=0" type="button"  class="btn btn-primary m-2 fload-end"  @click="updateClick(index)">Update</button>
+
+      </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+// const performSearch = (rows, term) => {
+//   const results = rows.filter(
+//     row => row.join(' ').toLowerCase().includes(term.toLowerCase())
+//   )
+//   return results
+
+
+// const sortByidTea = (d1,d2)=>(d1.idTea.toLowerCase()>d2.idTea.toLowerCase())?1:-1
+// const sortBynameTea = (d1,d2)=>(d1.nameTea.toLowerCase()>d2.nameTea.toLowerCase())?1:-1
+// const sortBysortTea = (d1,d2)=>(d1.sortTea.toLowerCase()>d2.sortTea.toLowerCase())?1:-1
+// const sortBypriceTea = (d1,d2)=>(d1.priceTea.toLowerCase()>d2.priceTea.toLowerCase())?1:-1
+
+export default {
+  data() {
+    return {
+      // updateIndex: 0,
+      teas:[],
+      currentSort:'name',
+      currentSortDir:'asc',
+      modalTitle:'',
+      idTea: 0,
+      nameTea:'',
+      sortTea:'',
+      priceTea:'',
+      descriptionTea:'',
+      idTeaFilter:'',
+      nameTeaFilter:'',
+      sortTeaFilter:'',
+      priceTeaFilter:'',
+      // sortParam:'',
+      withoutFilter:[],
+      products : [
+        { idTea: 1 , nameTea: 'Alishan', sortTea: 'oolong', priceTea: 5, descriptionTea:'some description' },
+        { idTea: 2, nameTea: 'Da Hong pao', sortTea: 'oolong', priceTea: 10, descriptionTea:'some description' },
+        { idTea: 3, nameTea: 'Shue', sortTea: 'puer', priceTea: 15, descriptionTea:'some description' },
+        { idTea: 4, nameTea: 'Alishan', sortTea: 'oolong', priceTea: 5, descriptionTea:'some description' },
+        { idTea: 5, nameTea: 'Da Hong pao', sortTea: 'oolong', priceTea: 10, descriptionTea:'some description' },
+      ],
+    }
+  },
+  computed:{
+    
+    sortedTeas() {
+      // switch(this.sortParam) {
+      //   case 'idTea':return this.products.sort(sortByidTea)
+      //   case 'nameTea':return this.products.sort(sortBynameTea)
+      //   case 'sortTea':return this.products.sort(sortBysortTea)
+      //   case 'priceTea':return this.products.sort(sortBypriceTea)
+      //   default: return this.products
+      // }
+      
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.products.sort((a,b)=>{
+        let modifier = 1
+        if(this.currentSortDir==='desc')modifier = -1
+        if(a[this.currentSort] < b[this.currentSort]) return -1*modifier
+        if(a[this.currentSort] > b[this.currentSort]) return 1*modifier
+        return 0
+      })
+    }
+  },
+
+  methods:{
+    sort(s) {
+      if(s=== this.currentSort) {
+        this.currentSortDir=this.currentSortDir==='asc'?'desc':'asc'
+      }
+      this.currentSort=s
+    },
+
+    addClick() {
+      this.modalTitle='Add Tea'
+      this.idTea=this.products.length + 1
+      this.nameTea=''
+      this.sortTea=''
+      this.priceTea=''
+      this.descriptionTea=''
+      
+    },
+    editClick(product) {
+      this.modalTitle='Edit Tea'
+      this.idTea=product.idTea
+      this.nameTea=product.nameTea
+      this.sortTea=product.sortTea
+      this.priceTea=product.priceTea
+      this.descriptionTea=product.descriptionTea
+      
+      // this.updateIndex = product.index
+      // this.product=this.products[product.index]
+
+    },
+    createClick() {
+      const { products, nameTea, sortTea, priceTea, descriptionTea } = this
+      this.products = [...products, {
+        nameTea,
+        sortTea,
+        priceTea,
+        descriptionTea
+      }]
+    },
+    updateClick(id) {
+      // this.products[this.this.updateIndex]=this.product
+      // this.product = {
+      //   nameTea: '',
+      //   sortTea: '',
+      //   priceTea: '',
+      //   description:''
+      // }
+    },
+    deleteClick(index) {
+      if(window.confirm('Do you really want to delete item?')) {
+        this.products.splice(index, 1)}
+    },
+
+    // computed:{
+    //   FilterFn() {
+    //     const { idTeaFilter, nameTeaFilter, sortTeaFilter, priceTeaFilter } = this
+
+    //     this.products=this.withoutFilter.filter(
+    //       (el) =>(el.idTea.toString().toLowerCase().includes(idTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.nameTea.toString().toLowerCase().includes(nameTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.sortTea.toString().toLowerCase().includes(sortTeaFilter.toString().trim().toLowerCase())
+    //       &&
+    //       el.priceTea.toString().toLowerCase().includes(priceTeaFilter.toString().trim().toLowerCase()))
+    //     )
+    //   },
+    // },
+
+    // Filter() {
+    //   const pr=this.priceTea
+    //   return this.products.filter((elem) => {
+    //     if(pr==='') return true
+    //     return elem.priceTea.indexOf(pr) > -1
+    //   })
+    // }
+
+
+    // sortResult(prop,asc) {
+    //   this.products=this.withoutFilter.sort((a,b) => {
+    //     if(asc) {
+    //       if (a[prop]>b[prop]) {
+    //         return 1
+    //       } 
+    //       if(a[prop]<b[prop]) {
+    //         return -1
+    //       } 
+    //       return 0
+            
+    //     } 
+    //     if ((b[prop]>a[prop])) {
+    //       return 1
+    //     } 
+    //     if ((b[prop]<a[prop])) {
+    //       return -1
+    //     } 
+    //     return 0     
+    //   })
+    // },
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <template>
   <div>
     <div id="demo" class="container">
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,104 @@
 import { createApp } from 'vue'
+
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-blue/theme.css'       // theme
+import 'primevue/resources/primevue.min.css'                 // core css
+import 'primeicons/primeicons.css'                           // icons
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'     // optional for column grouping
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
+import FileUpload from 'primevue/fileupload'
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Textarea from 'primevue/textarea'
+import RadioButton from 'primevue/radiobutton'
+import Dropdown from 'primevue/dropdown'
+import Dialog from 'primevue/dialog'
+import Rating from 'primevue/rating'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+import AutoComplete from 'primevue/autocomplete'
+import Accordion from 'primevue/accordion'
+import Avatar from 'primevue/avatar'
+import AvatarGroup from 'primevue/avatargroup'
+import Badge from 'primevue/badge'
+import BlockUI from 'primevue/blockui'
+import Breadcrumb from 'primevue/breadcrumb'
+import Calendar from 'primevue/calendar'
+import Card from 'primevue/card'
+import CascadeSelect from 'primevue/cascadeselect'
+import Carousel from 'primevue/carousel'
+import Checkbox from 'primevue/checkbox'
+import Chip from 'primevue/chip'
+import Chips from 'primevue/chips'
+import ColorPicker from 'primevue/colorpicker'
+import ConfirmDialog from 'primevue/confirmdialog'
+import ConfirmPopup from 'primevue/confirmpopup'
+import ConfirmationService from 'primevue/confirmationservice'
+import ContextMenu from 'primevue/contextmenu'
+import DataView from 'primevue/dataview'
+import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions'
+import DeferredContent from 'primevue/deferredcontent'
+import Divider from 'primevue/divider'
+import Dock from 'primevue/dock'
+import Fieldset from 'primevue/fieldset'
+import Galleria from 'primevue/galleria'
+import Image from 'primevue/image'
+import InlineMessage from 'primevue/inlinemessage'
+import Inplace from 'primevue/inplace'
+import InputSwitch from 'primevue/inputswitch'
+import InputMask from 'primevue/inputmask'
+import Knob from 'primevue/knob'
+import Listbox from 'primevue/listbox'
+import MegaMenu from 'primevue/megamenu'
+import Menu from 'primevue/menu'
+import Menubar from 'primevue/menubar'
+import Message from 'primevue/message'
+import MultiSelect from 'primevue/multiselect'
+import OrderList from 'primevue/orderlist'
+import OrganizationChart from 'primevue/organizationchart'
+import OverlayPanel from 'primevue/overlaypanel'
+import Paginator from 'primevue/paginator'
+import Panel from 'primevue/panel'
+import PanelMenu from 'primevue/panelmenu'
+import Password from 'primevue/password'
+import PickList from 'primevue/picklist'
+import ProgressBar from 'primevue/progressbar'
+import ProgressSpinner from 'primevue/progressspinner'
+import Ripple from 'primevue/ripple'
+import Row from 'primevue/row'
+import SelectButton from 'primevue/selectbutton'
+import ScrollPanel from 'primevue/scrollpanel'
+import ScrollTop from 'primevue/scrolltop'
+import Skeleton from 'primevue/skeleton'
+import Slider from 'primevue/slider'
+import Sidebar from 'primevue/sidebar'
+import SpeedDial from 'primevue/speeddial'
+import SplitButton from 'primevue/splitbutton'
+import Splitter from 'primevue/splitter'
+import SplitterPanel from 'primevue/splitterpanel'
+import Steps from 'primevue/steps'
+import StyleClass from 'primevue/styleclass'
+import TabMenu from 'primevue/tabmenu'
+import TieredMenu from 'primevue/tieredmenu'
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
+import Tag from 'primevue/tag'
+import Terminal from 'primevue/terminal'
+import Timeline from 'primevue/timeline'
+import ToggleButton from 'primevue/togglebutton'
+import Tooltip from 'primevue/tooltip'
+import Tree from 'primevue/tree'
+import TreeSelect from 'primevue/treeselect'
+import TreeTable from 'primevue/treetable'
+import TriStateCheckbox from 'primevue/tristatecheckbox'
+import VirtualScroller from 'primevue/virtualscroller'
+
+
+
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
@@ -8,7 +108,7 @@ import App from './App.vue'
 import router from './router'
 
 
-globalCookiesConfig({
+globalCookiesConfig({ 
   expireTimes: '30d',
   path: '/',
   domain: '',
@@ -16,10 +116,40 @@ globalCookiesConfig({
   sameSite: 'None',
 })
 
-createApp(App)
+const app = createApp(App)
+app
   .use(router)
   .use(BootstrapVue3)
+  .use(PrimeVue)
   .use(useCookies)
   .use(store)
-  .mount('#app')
+
+  .use(ToastService)
+  .component('Toast', Toast)
+  .component('DataTable', DataTable)
+  .component('Column', Column)
+  .component('ColumnGroup', ColumnGroup)
+  .component('Toolbar', Toolbar)
+  .component('Button', Button)
+  .component('FileUpload', FileUpload)
+  .component('InputText', InputText)
+  .component('InputNumber', InputNumber)
+  .component('Textarea', Textarea)
+  .component('RadioButton', RadioButton)
+  .component('Dropdown', Dropdown)
+  .component('Dialog', Dialog)
+  .component('Rating', Rating)
+
+
+
+
+
+
+
+
   
+
+  .mount('#app')
+
+  
+
