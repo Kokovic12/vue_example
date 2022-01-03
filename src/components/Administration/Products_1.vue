50cd29aae27f1136c34575fc15f621d822b13943
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2022-01-03 17:39:11 +0300

    feat:complete CRUD

--- src/components/Administration/Products_1.vue
+++ src/components/Administration/Products_1.vue
@@ -1,3 +1,297 @@
+<template>
+  <div>
+    <div class="card">
+      <Toolbar class="p-mb-4">
+                <template #start>
+                    <Button label="New" icon="pi pi-plus" class="p-button-success p-mr-2" @click="openNew" />
+                    <Button label="Delete" icon="pi pi-trash" class="p-button-danger" :disabled="!selectedProducts || !selectedProducts.length" @click="confirmDeleteSelected" />
+                </template>
+
+                <template #end>
+                    <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" chooseLabel="Import" class="p-mr-2 p-d-inline-block" />
+                    <Button label="Export" icon="pi pi-upload" class="p-button-help" @click="exportCSV($event)"  />
+                </template>
+            </Toolbar>
+
+
+      <DataTable ref="dt" :value="products" v-model:selection="selectedProducts" dataKey="id"
+                    :paginator="true" :rows="12" :filters="filters"  
+                    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[5,10,25]"
+                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products" responsiveLayout="scroll"
+                    >
+                    
+          <template #header>
+            <div class="table-header p-d-flex p-flex-column p-flex-md-row p-jc-md-between">
+						  <h5 class="p-mb-2 p-m-md-0 p-as-md-center">Manage Products</h5>
+						  <span class="p-input-icon-left">
+                <i class="pi pi-search" />
+                  <InputText v-model="filters['global'].value" placeholder="Search..." />
+              </span>
+					  </div>
+          </template>
+
+            <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
+            <Column field="code" header="Code" :sortable="true" style="min-width:12rem"></Column>
+            <Column field="name" header="Name" :sortable="true" style="min-width:12rem">
+              <!-- <template #editor="slotProps">
+                <InputText v-model="slotProps.data[slotProps.field]" autofocus/>
+              </template> -->
+            </Column>
+            <Column field="sort" header="Sort" :sortable="true" style="min-width:10rem"></Column>
+            <Column field="price" header="Price" :sortable="true" style="min-width:8rem"></Column>
+            <Column field="description" header="Description" :sortable="true" style="min-width:12rem"></Column>
+            <Column :exportable="false" style="min-width:8rem">
+                    <template #body="slotProps">
+                        <Button icon="pi pi-pencil" class="p-button-rounded p-button-success p-mr-2" @click="editProduct(slotProps.data)" />
+                        <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="confirmDeleteProduct(slotProps.data)" />
+                    </template>
+                </Column>
+        </DataTable>
+      </div>
+
+      <Dialog v-model:visible="productDialog" :style="{width: '450px'}" header="Product Details" :modal="true" class="p-fluid">
+            <div class="p-field">
+                <label for="name">Name</label>
+                <InputText id="name" v-model.trim="product.name" required="true" autofocus :class="{'p-invalid': submitted && !product.name}" />
+                <small v-if="submitted && !product.name" class="p-error">Name is required.</small>
+            </div>
+
+            <div class="p-field">
+                <label for="sort">Sort</label>
+                <InputText id="sort" v-model.trim="product.sort" required="true" autofocus :class="{'p-invalid': submitted && !product.sort}" />
+                <small v-if="submitted && !product.sort" class="p-error">Sort is required.</small>
+            </div>
+
+            <div class="p-field">
+                <label for="price">Price</label>
+                <InputText id="price" v-model.trim="product.price" required="true" autofocus :class="{'p-invalid': submitted && !product.price}" />
+                <small v-if="submitted && !product.price" class="p-error">Price is required.</small>
+            </div>
+
+            <div class="p-field">
+                <label for="description">Description</label>
+                <Textarea id="description" v-model="product.description" required="true" rows="3" cols="20" />
+            </div>
+
+            <template #footer>
+                <Button label="Cancel" icon="pi pi-times" class="p-button-text" @click="hideDialog"/>
+                <Button label="Save" icon="pi pi-check" class="p-button-text" @click="saveProduct" />
+            </template>
+        </Dialog>
+
+        <Dialog v-model:visible="deleteProductDialog" :style="{width: '450px'}" header="Confirm" :modal="true">
+            <div class="confirmation-content">
+                <i class="pi pi-exclamation-triangle p-mr-3" style="font-size: 2rem" />
+                <span v-if="product">Are you sure you want to delete <b>{{product.name}}</b>?</span>
+            </div>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteProductDialog = false"/>
+                <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteProduct" />
+            </template>
+        </Dialog>
+
+        <Dialog v-model:visible="deleteProductsDialog" :style="{width: '450px'}" header="Confirm" :modal="true">
+            <div class="confirmation-content">
+                <i class="pi pi-exclamation-triangle p-mr-3" style="font-size: 2rem" />
+                <span v-if="product">Are you sure you want to delete the selected products?</span>
+            </div>
+            <template #footer>
+                <Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteProductsDialog = false"/>
+                <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="deleteSelectedProducts" />
+            </template>
+        </Dialog>
+    </div>
+</template>
+
+
+<script>
+import {
+  ref,
+  onMounted,
+  defineComponent
+} from 'vue'
+import { FilterMatchMode } from 'primevue/api'
+import { useToast } from 'primevue/usetoast'
+import ProductService from '@/services/ProductService'
+
+export default defineComponent ({
+  setup() {
+    const productService = ref(new ProductService())
+    const products = ref()
+
+    onMounted(() => {
+      // eslint-disable-next-line no-return-assign
+      productService.value.getProducts().then(data => products.value = data)
+    })
+
+    const toast = useToast()
+    const dt = ref()
+    const productDialog = ref(false)
+    const deleteProductDialog = ref(false)
+    const deleteProductsDialog = ref(false)
+    const product = ref({})
+    const selectedProducts = ref()
+    const submitted = ref(false)    
+    const filters = ref({
+      'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+    })
+
+    const openNew = () => {
+      product.value = {}
+      submitted.value = false
+      productDialog.value = true
+    }
+
+    const hideDialog = () => {
+      productDialog.value = false
+      submitted.value = false
+    }
+
+    const findIndexById = (id) => {
+      let index = -1
+      for (let i = 0; i < products.value.length; i+1) {
+        if (products.value[i].id === id) {
+          index = i
+          break
+        }
+      }
+
+      return index
+    }
+
+    const createId = () => {
+      let id = ''
+      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      for ( let i = 0; i < 5; i+1 ) {
+        id += chars.charAt(Math.floor(Math.random() * chars.length))
+      }
+      return id
+    }
+
+
+    
+    const saveProduct = () => {
+      submitted.value = true
+
+      if (product.value.name.trim()) {
+        if (product.value.id) {
+          product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus
+          products.value[findIndexById(product.value.id)] = product.value
+          toast.add({ severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000 })
+        }
+        else {
+          product.value.id = createId()
+          // product.value.code = createId()
+          products.value.push(product.value)
+          toast.add({ severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000 })
+        }
+
+        productDialog.value = false
+        product.value = {}
+      }
+    }
+
+    const editProduct = (prod) => {
+      product.value = { ...prod }
+      productDialog.value = true
+    }
+
+    const confirmDeleteProduct = (prod) => {
+      product.value = prod
+      deleteProductDialog.value = true
+    }
+    const deleteProduct = () => {
+      products.value = products.value.filter(val => val.id !== product.value.id)
+      deleteProductDialog.value = false
+      product.value = {}
+      toast.add({ severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 })
+    }
+    const exportCSV = () => {
+      dt.value.exportCSV()
+    }
+    const confirmDeleteSelected = () => {
+      deleteProductsDialog.value = true
+    }
+    const deleteSelectedProducts = () => {
+      products.value = products.value.filter(val => !selectedProducts.value.includes(val))
+      deleteProductsDialog.value = false
+      selectedProducts.value = null
+      toast.add({ severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 })
+    }
+
+    return { saveProduct, products, productService,deleteSelectedProducts, confirmDeleteSelected, exportCSV, createId, findIndexById, deleteProduct, confirmDeleteProduct, editProduct, hideDialog, openNew, filters }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.table-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media screen and (max-width: 960px) {
+        align-items: start;
+	}
+}
+
+.product-image {
+    width: 50px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.p-dialog .product-image {
+    width: 50px;
+    margin: 0 auto 2rem auto;
+    display: block;
+}
+
+.confirmation-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+@media screen and (max-width: 960px) {
+	::v-deep(.p-toolbar) {
+		flex-wrap: wrap;
+        
+		.p-button {
+            margin-bottom: 0.25rem;
+        }
+	}
+}
+</style>
+
+
+{
+    "data":[
+        { "id": 1, "code": "f230fh0g3", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 2, "code": "nvklal433", "name": "Da Hong pao", "sort": "oolong", "price": 10, "description":"some description" },
+        { "id": 3, "code": "zz21cz3c1", "name": "Shue", "sort": "puer", "price": 15, "description":"some description" },
+        { "id": 4, "code": "244wgerg2", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 5, "code": "h456wer53", "name": "Da Hong pao", "sort": "red", "price": 10, "description":"some description" },
+        { "id": 6, "code": "av2231fwg", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 7, "code": "bib36pfvm", "name": "Da Hong pao", "sort": "oolong", "price": 10, "description":"some description" },
+        { "id": 8, "code": "mbvjkgip5", "name": "Shue", "sort": "puer", "price": 15, "description":"some description" },
+        { "id": 9, "code": "vbb124btr", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 10, "code": "cm230f032", "name": "Da Hong pao", "sort": "yellow ", "price": 10, "description":"some description" },
+        { "id": 11, "code": "plb34234v", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 12, "code": "4920nnc2d", "name": "Da Hong pao", "sort": "red", "price": 14, "description":"some description" },
+        { "id": 13, "code": "250vm23cc", "name": "Shue", "sort": "puer", "price": 15, "description":"some description" },
+        { "id": 14, "code": "fldsmn31b", "name": "Alishan", "sort": "oolong", "price": 5, "description":"some description" },
+        { "id": 15, "code": "waas1x2as", "name": "Da Hong pao", "sort": "oolong", "price": 10, "description":"some description" },
+        { "id": 16, "code": "vb34btbg5", "name": "Alishan", "sort": "yellow ", "price": 7, "description":"some description" },
+        { "id": 17, "code": "k8l6j58jl", "name": "Da Hong pao", "sort": "oolong", "price": 11, "description":"some description" },
+        { "id": 18, "code": "v435nn85n", "name": "Shue", "sort": "puer", "price": 15, "description":"some description" },
+        { "id": 19, "code": "09zx9c0zc", "name": "Alishan", "sort": "red", "price": 5, "description":"some description" },
+        { "id": 20, "code": "mnb5mb2m5", "name": "Da Hong pao", "sort": "oolong", "price": 18, "description":"some description" }
+
+
+    ]
+}
+
+
+
 
 
 
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,20 +1,8 @@
 export default class ProductService {
   res: Response | undefined
 
-  async getProductsSmall():Promise<any> {
-    this.res = await fetch('/products-small.json')
-    const d = await this.res.json()
-    return d.data
-  }
-
   async getProducts():Promise<any> {
     this.res = await fetch('/products.json')
-    const d = await this.res.json()
-    return d.data
-  }
-
-  async getProductsWithOrdersSmall():Promise<any> {
-    this.res = await fetch('/products-orders-small.json')
     const d = await this.res.json()
     return d.data
   }
